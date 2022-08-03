b6e4af1bb6eb3062f0203f50a85ec76035ef1e75
axeljk <axeljkern@yahoo.com>
2022-08-03 01:08:29 -0400

    Adds foreign keys to ProductTag.

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -11,6 +11,20 @@ ProductTag.init(
 		allowNull: false,
 		primaryKey: true,
 		autoIncrement: true
+	},
+	product_id: {
+		type: DataTypes.INTEGER,
+		references: {
+			model: 'product',
+			key: 'id'
+		}
+	},
+	tag_id: {
+		type: DataTypes.INTEGER,
+		references: {
+			model: 'tag',
+			key: 'id'
+		}
 	}
   },
   {
